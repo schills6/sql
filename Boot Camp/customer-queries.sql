a335848089b0469dee0b2c36456534096498da0f
Erin Heinrich <schills@gmail.com>
2019-04-16 16:36:02 -0400

    folder moves

--- Boot Camp/customer-queries.sql
+++
@@ -1,6 +0,0 @@
-SELECT * 
-	FROM customer 
-    where sales >30000000 
-    and sales <70000000
-    ORDER BY Sales desc; 
-    -- WHERE State = 'HI';
--- Bootcamp/customer-queries.sql
+++ Bootcamp/customer-queries.sql
@@ -3,4 +3,6 @@ SELECT *
     where sales >30000000 
     and sales <70000000
     ORDER BY Sales desc; 
-    -- WHERE State = 'HI';
+    
+    select name as 'Customer Name', sales as 'Total Sales'
+    from customer;
