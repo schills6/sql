6086105bf160b9d90e82911f2df978e3eb68ecb7
Erin Heinrich <schills@gmail.com>
2019-04-17 17:48:26 -0400

    fixing column titles

--- Bootcamp/mma/mmajoin3.sql
+++ Bootcamp/mma/mmajoin3.sql
@@ -1,4 +1,5 @@
-select i.orderdate, i.customername, p.description, l.quantity, p.price
+select i.orderdate as 'Order Date', i.ordernumber as 'Order Number', i.id as 'Customer ID',
+ i.customername as 'Customer Name', p.description as 'Title', l.quantity as 'Qty', p.price as 'Price'
 	from lineitem l
 		inner join 
 			product p 
