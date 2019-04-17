5e4d0165199592a94c271c48927d79ec4a6827a1
Erin Heinrich <schills@gmail.com>
2019-04-17 13:57:17 -0400

    editing folder structure

--- Bootcamp/customer-queries.sql
+++
@@ -1,8 +0,0 @@
-SELECT * 
-	FROM customer 
-    where sales >30000000 
-    and sales <70000000
-    ORDER BY Sales desc; 
-    
-    select name as 'Customer Name', sales as 'Total Sales'
-    from customer;
--- Bootcamp/customerconcatandformatexample.sql
+++
@@ -1,4 +0,0 @@
-SELECT name as 'contact name', 
-		concat(city, ', ', state) as 'location', 
-		concat ('$', format (sales,2)) as 'sales' 
-FROM customer;
