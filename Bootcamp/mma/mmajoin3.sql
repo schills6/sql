a29079851ff4ae3ac9ef3d1b467bdc8dd4625f7c
Erin Heinrich <schills@gmail.com>
2019-04-17 17:44:03 -0400

    clean script

--- Bootcamp/mma/mmajoin3.sql
+++ Bootcamp/mma/mmajoin3.sql
@@ -1,4 +1,8 @@
 select i.orderdate, i.customername, p.description, l.quantity, p.price
 	from lineitem l
-		inner join product p on p.ID = l.productID
-        inner join invoice i on i.ID = l.invoiceID;
+		inner join 
+			product p 
+				on p.ID = l.productID
+        inner join 
+			invoice i
+				on i.ID = l.invoiceID;
