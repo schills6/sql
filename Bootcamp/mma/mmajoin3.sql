0db04ad8a1d175c3b59d60f3644d1c311d4d167c
Erin Heinrich <schills@gmail.com>
2019-04-18 09:12:36 -0400

    updating table alias

--- Bootcamp/mma/mmajoin3.sql
+++ Bootcamp/mma/mmajoin3.sql
@@ -1,9 +1,10 @@
-select i.orderdate as 'Order Date', i.ordernumber as 'Order Number', i.id as 'Customer ID',
- i.customername as 'Customer Name', p.description as 'Title', l.quantity as 'Qty', p.price as 'Price'
-	from lineitem l
+select i.orderdate as 'Order Date', i.ordernumber as 'Order Number', 
+ i.customername as 'Customer Name', p.code as 'Product Code', p.description as 'Title',
+ li.quantity as 'Qty', p.price as 'Price'
+	from lineitem li
 		inner join 
 			product p 
-				on p.ID = l.productID
+				on p.ID = li.productID
         inner join 
 			invoice i
-				on i.ID = l.invoiceID;
+				on i.ID = li.invoiceID;
